b006f9eae8a45183b8ac5f72978dac9e23eb9ae9
Khasan <ixanaxy@gmail.com>
2022-04-06 10:54:31 +0500

    Initial Commit

---
+++ FirePerfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirePerfect
+//
+//  Created by Xasan Xasanov on 06/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirePerfect/FirePerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirePerfectApp.swift
+//  FirePerfect
+//
+//  Created by Xasan Xasanov on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FirePerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
